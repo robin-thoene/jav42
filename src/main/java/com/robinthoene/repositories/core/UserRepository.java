27645eeddc241fc262c2538857114a9295af2749
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2022-09-14 20:43:47 +0200

    implement the REST facade of the core api

--- src/main/java/com/robinthoene/repositories/core/UserRepository.java
+++ src/main/java/com/robinthoene/repositories/core/UserRepository.java
@@ -1,6 +1,45 @@
 package com.robinthoene.repositories.core;
 
 import com.robinthoene.logic.interfaces.IUserRepository;
+import com.robinthoene.logic.models.UserModel;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+/**
+ * Repository regarding users.
+ */
+@Component
 public class UserRepository implements IUserRepository {
+
+    /**
+     * Default constructor.
+     */
+    public UserRepository() {
+    }
+
+    @Override
+    public UserModel getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserModel createUser(UserModel createModel) {
+        return null;
+    }
+
+    @Override
+    public UserModel updateUser(UserModel updateModel) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(long id) {
+
+    }
 }

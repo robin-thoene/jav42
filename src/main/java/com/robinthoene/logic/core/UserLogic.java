16a526c4455db8cc53b4e43d54ee63e715d0fd78
robin-thoene <robin.thoene@posteo.de>
2022-09-14 20:43:47 +0200

    implement the REST facade of the core api

--- src/main/java/com/robinthoene/logic/core/UserLogic.java
+++ src/main/java/com/robinthoene/logic/core/UserLogic.java
@@ -1,6 +1,54 @@
 package com.robinthoene.logic.core;
 
 import com.robinthoene.logic.interfaces.IUserLogic;
+import com.robinthoene.logic.interfaces.IUserRepository;
+import com.robinthoene.logic.models.UserModel;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+/**
+ * Logic regarding users.
+ */
+@Component
 public class UserLogic implements IUserLogic {
+
+    /**
+     * Default constructor.
+     *
+     * @param userRepository The injected user repository.
+     */
+    public UserLogic(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserModel getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> getAll() {
+        return null;
+    }
+
+    @Override
+    public UserModel createUser(UserModel createModel) {
+        return null;
+    }
+
+    @Override
+    public UserModel updateUser(UserModel updateModel) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(long id) {
+
+    }
+
+    /**
+     * The injected user repository.
+     */
+    private IUserRepository userRepository;
 }
--- src/main/java/com/robinthoene/services/coreapi/CoreapiApplication.java
+++ src/main/java/com/robinthoene/services/coreapi/CoreapiApplication.java
@@ -3,9 +3,14 @@ package com.robinthoene.services.coreapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.robinthoene.logic.interfaces", "com.robinthoene.repositories.core", "com.robinthoene.logic.core", "com.robinthoene.services.coreapi"})
 public class CoreapiApplication {
 
+    /**
+     * The main entry point for the core api.
+     *
+     * @param args The optional start arguments.
+     */
     public static void main(String[] args) {
         SpringApplication.run(CoreapiApplication.class, args);
     }

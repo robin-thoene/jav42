04cc4d958914aeec0bc742086552e368082d59b0
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2022-09-18 09:37:45 +0200

    implement the password check to authenticate a user

--- src/main/java/com/robinthoene/jav42/logic/authorization/UserAuthorizationLogic.java
+++ src/main/java/com/robinthoene/jav42/logic/authorization/UserAuthorizationLogic.java
@@ -2,7 +2,10 @@ package com.robinthoene.jav42.logic.authorization;
 
 import com.robinthoene.jav42.logic.common.exceptions.UserAuthenticationException;
 import com.robinthoene.jav42.logic.interfaces.IUserAuthorizationLogic;
+import com.robinthoene.jav42.logic.interfaces.IUserRepository;
 import com.robinthoene.jav42.logic.models.UserReadModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,13 +13,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class UserAuthorizationLogic implements IUserAuthorizationLogic {
-    @Override
-    public UserReadModel AuthenticateUserWithPassword(String userName, String password) throws UserAuthenticationException {
-        return null;
-    }
 
     @Override
-    public UserReadModel AuthenticateUserWithPasswordHash(String userName, String passwordHash) throws UserAuthenticationException {
-        return null;
+    public UserReadModel authenticateUserWithPasswordHash(String userName, String passwordHash) throws UserAuthenticationException {
+        var userToCheck = userRepository.getByUserName(userName);
+        var isPasswordValid = userRepository.checkUserPassword(userToCheck, passwordHash);
+        if (!isPasswordValid) {
+            throw new UserAuthenticationException();
+        }
+        return userToCheck;
     }
+
+    @Autowired
+    @Lazy
+    private IUserRepository userRepository;
 }

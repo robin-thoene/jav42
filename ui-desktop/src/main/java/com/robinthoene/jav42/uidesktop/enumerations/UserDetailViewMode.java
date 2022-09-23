7486ed5d4edf7dfccf9fcbfd1ecb8c1183a13328
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2022-09-23 18:11:51 +0200

    handle different modi of the user detail view

---
+++ ui-desktop/src/main/java/com/robinthoene/jav42/uidesktop/enumerations/UserDetailViewMode.java
@@ -0,0 +1,10 @@
+package com.robinthoene.jav42.uidesktop.enumerations;
+
+/**
+ * The possible states for the user detail view.
+ */
+public enum UserDetailViewMode {
+    VIEW,
+    CREATE,
+    EDIT
+}

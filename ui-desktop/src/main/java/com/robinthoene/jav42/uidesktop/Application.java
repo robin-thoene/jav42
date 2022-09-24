8ec23783b943605aaf8c8d1b68a6395bc9871ce1
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2022-09-24 16:04:42 +0200

    users can be edited

--- ui-desktop/src/main/java/com/robinthoene/jav42/uidesktop/Application.java
+++ ui-desktop/src/main/java/com/robinthoene/jav42/uidesktop/Application.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
+        stage.setResizable(false);
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("JAB42 - Client Application");

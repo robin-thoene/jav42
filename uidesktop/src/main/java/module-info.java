f72816b23b1422a46f817005b6dbcfaa1ccf4adb
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2022-09-25 13:29:27 +0200

    create container image for the core api

--- uidesktop/src/main/java/module-info.java
+++ uidesktop/src/main/java/module-info.java
@@ -2,9 +2,9 @@ module com.robinthoene.jav42.uidesktop {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.net.http;
-    requires com.robinthoene.jav42.common;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
+    requires com.robinthoene.jav42.common;
 
     opens com.robinthoene.jav42.uidesktop to javafx.fxml;
     exports com.robinthoene.jav42.uidesktop;
